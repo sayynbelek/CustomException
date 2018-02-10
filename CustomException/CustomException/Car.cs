e4ce3f387ecd26cb171f0ec8b5f035efb3afaafa
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-10 10:21:19 +0700

    Add

---
+++ CustomException/CustomException/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    public class CarIsDead : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+        public void CarIsDeadException() { }
+        public void CarIsDeadException(string message, string cause, DateTime time)
+        {
+            messageDetails = message;
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Car Error Message: {0}", messageDetails);
+            }
+        }
+    }
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+                CurrentSpeed += delta;
+            if (CurrentSpeed > MaxSpeed)
+            {
+                carIsDead = true;
+                CurrentSpeed = 0;
+                CarIsDeadException ex = new CarIsDeadException(string.Format("{0} has overheated!", PetName),
+                    "You have a lead foot", DateTime.Now);
+                ex.HelpLink = "http://www.CarsRUs.com";
+                ex.Data.Add("TimeStamp", string.Format("The car exploded at {0}", DateTime.Now));
+                ex.Data.Add("Cause", "You hav a lead foot");
+                throw ex;
+            }
+            else
+                Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+        }
+    }
+}
---
+++ CustomException/CustomException/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with custom Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(50);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    [Serializable]
+    internal class CarIsDeadException : Exception
+    {
+        private DateTime now;
+        private string v;
+
+        public CarIsDeadException()
+        {
+        }
+
+        public CarIsDeadException(string message) : base(message)
+        {
+        }
+
+        public CarIsDeadException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public CarIsDeadException(string message, string v, DateTime now) : this(message)
+        {
+            this.v = v;
+            this.now = now;
+        }
+
+        protected CarIsDeadException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public bool CauseOfError { get; internal set; }
+        public bool ErrorTimeStamp { get; internal set; }
+    }
+}
